800aa9297dc5f1a3cbc8fc609957691da0d2cfd6
Nur Abdu <nur.94@hotmail.it>
2020-11-27 19:59:13 +0100

    modify both extraction table and results format

--- lotto/extraction.py
+++ lotto/extraction.py
@@ -15,12 +15,13 @@ class Extraction:
                 self.table[city] = Numbers(numbers_to_extract)
 
     def print_extracted(self):
-        extraction_table_line = '+' + '-' * 28 + '+'
+        extraction_table_line = '+' + '-' * 48 + '+'
         print(extraction_table_line)
-        print('|{:^28}|'.format('EXTRACTION'))
+        print('|{:^48}|'.format('EXTRACTION'))
         print(extraction_table_line)
         for city in self.table.keys():
-                print('|{:10}{:^18}|'.format(city, ' '.join([str(x) for x in self.table[city].generated_numbers])))
+            extracted_str = ' '.join([str(x) for x in self.table[city].generated_numbers])
+            print('|  {:<10}{:^7}{:^7}{:^7}{:^7}{:^7} |'.format(city, *extracted_str.split()))
         print(extraction_table_line)
 
     def results(self, ticket):
@@ -42,11 +43,11 @@ class Extraction:
         for city in win_dict.keys():
             if len(win_dict[city]) > Bet.bet_type_list.index(ticket.bet_type.selected_bet_type):
                 print()
-                print(' {:^48} '.format('CONGRATULATIONS!'))
-                print(' {:^48} '.format('YOU WIN'))
+                print(' {::^48} '.format('  CONGRATULATIONS! YOU WIN!  '))
                 ticket.print_ticket()
-                print('|{:^48}|'.format('WINNER CITY : '+city))
-                print('|{:^48}|'.format((' '.join(str(x) for x in win_dict[city]))))
+                print('+{:^48}+'.format('WINNER CITY : '+city))
+                print('+{:^48}+'.format((' '.join(str(x) for x in win_dict[city]))))
                 print('+{:-^48}+'.format(''))
+                print()
 
 
